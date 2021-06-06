a50d4d9202a47cf77c28080be737c8861c03cf21
C-Monster-Boy <aranuj98@gmail.com>
2021-06-07 00:02:36 +0530

    Corrections

--- Last Man To Break/Assets/Scripts/UI_LobbyUIHandler.cs
+++ Last Man To Break/Assets/Scripts/UI_LobbyUIHandler.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class UI_LobbyUIHandler : MonoBehaviour
 {
-#region 
     // Start is called before the first frame update
     void Start()
     {
         
     }
+
 
     // Update is called once per frame
     void Update()
